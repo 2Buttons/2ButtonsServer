db5be2e3b5a7f2ce40fbab168e2536baeead7b98
Yuriy Aksenov <Yura290610@yandex.ru>
2018-06-12 22:20:45 +0300

    Добавил зависимости доп в Media and Monitring

--- MonitoringServer/MonitoringServer/Program.cs
+++ MonitoringServer/MonitoringServer/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using Serilog.Events;
 
 namespace MonitoringServer
