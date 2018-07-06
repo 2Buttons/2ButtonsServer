a1438c809a2f629a23120b13bf4135cdef5f5bbb
Yuriy Aksenov <Yura290610@yandex.ru>
2018-07-06 17:19:13 +0300

    Начато Добавление бот сервера

---
+++ BotsServer/BotsServer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonLibraries.CommandLine;
+using CommonLibraries.Extensions;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace BotsServer
+{
+  public class Program
+  {
+    public static IConfiguration Configuration { get; private set; }
+    public static string Scheme { get; } = "http";
+    public static string IpAddress { get; } = "localhost";
+    public static string Port { get; private set; }
+    public static string Url => Scheme + "://" + IpAddress + ":" + Port;
+
+    public static void Main(string[] args)
+    {
+      BuildWebHost(args).Run();
+    }
+
+    public static IWebHost BuildWebHost(string[] args)
+    {
+      var builder = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory);
+
+      builder.AddJsonFile(Path.Combine(AppContext.BaseDirectory, "appsettings.json"));
+      builder.AddJsonFile(Path.Combine(AppContext.BaseDirectory, "..", "commonsettings.json"));
+
+      Configuration = builder.Build();
+      Port = Configuration["WebHost:Port"];
+
+      var commandLine = Command.WithName("host").HasOption("-port", "-p");
+
+      if (commandLine.TryParse(args, out var command) && command is HostCommand hostCommand &&
+          !hostCommand.Port.IsNullOrEmpty()) Port = hostCommand.Port;
+
+      return WebHost.CreateDefaultBuilder(args).UseConfiguration(Configuration).UseUrls(Url)
+        .ConfigureLogging(logging => logging.SetMinimumLevel(LogLevel.Trace)).UseStartup<Startup>().UseSerilog(
+          (ctx, cfg) =>
+          {
+            var serverName = Configuration.GetValue<string>("ServerName");
+            var path = Path.Combine(AppContext.BaseDirectory, ".." + Path.DirectorySeparatorChar, "Logs",
+              serverName + "{Date}" + ".log");
+
+            cfg.ReadFrom.Configuration(ctx.Configuration).MinimumLevel.Debug().MinimumLevel
+              .Override("Microsoft", LogEventLevel.Information).WriteTo.RollingFile(path,
+                outputTemplate:
+                "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Application} {Level:u3}] {SourceContext}: {Message:lj}{NewLine}{Exception}");//,fileSizeLimitBytes:500*1024*1024);
+          }).Build();
+    }
+  }
+}
---
+++ BotsServer/BotsServer/Startup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonLibraries;
+using CommonLibraries.ConnectionServices;
+using CommonLibraries.Exceptions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BotsServer
+{
+  public class Startup
+  {
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+      services.AddCors(options =>
+      {
+        options.AddPolicy("AllowAllOrigin", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+      });
+      services.AddDbContext<TwoButtonsContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TwoButtonsConnection")));
+      services.Configure<ConnectionStrings>(options => options.TwoButtonsConnection = Configuration.GetConnectionString("TwoButtonsConnection"));
+      services.AddTransient<QuestionsUnitOfWork>();
+      services.AddOptions();
+      services.Configure<ServersSettings>(Configuration.GetSection("ServersSettings"));
+      services.AddConnectionsHub();
+
+
+      var jwtAppSettingOptions = Configuration.GetSection(nameof(JwtSettings));
+      var secretKey = jwtAppSettingOptions["SecretKey"];
+      var issuer = jwtAppSettingOptions[nameof(JwtSettings.Issuer)];
+      var audience = jwtAppSettingOptions[nameof(JwtSettings.Audience)];
+
+      services.Configure<JwtSettings>(options =>
+      {
+        options.Issuer = issuer;
+        options.Audience = audience;
+        options.SigningCredentials = new SigningCredentials(JwtSettings.CreateSecurityKey(secretKey),
+          SecurityAlgorithms.HmacSha256);
+      });
+
+      services.AddAuthentication(options =>
+      {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+      }).AddJwtBearer(configureOptions =>
+      {
+        configureOptions.ClaimsIssuer = issuer;
+        configureOptions.RequireHttpsMetadata = false;
+        configureOptions.TokenValidationParameters =
+          JwtSettings.CreateTokenValidationParameters(issuer, audience, JwtSettings.CreateSecurityKey(secretKey));
+      });
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      if (env.IsDevelopment()) loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+      loggerFactory.AddDebug();
+      app.UseExceptionHandling();
+
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+
+      app.UseForwardedHeaders(new ForwardedHeadersOptions
+      {
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+      });
+
+      app.UseAuthentication();
+      app.UseMvc();
+    }
+  }
+}
